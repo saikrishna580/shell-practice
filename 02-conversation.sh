bddda0e707b1522b0c83b4273eedd22c9ec1cb36
saikowru <sai@devops.com>
2025-12-08 02:31:34 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,5 +3,5 @@
 echo "Trump:: Hey Musk, How are you?"
 echo "Musk":: Hello Trump, I am fine. how are you doing"
 echo "Trump":: I am fine too. whats's up?
-echo "Musk" :: Nothing, just going to mars now, will you come"
-echo "Trump" :: soory, you carry on! I will come once you come bacck"
+echo "Musk":: Nothing, just going to mars now, will you come"
+echo "Trump":: soory, you carry on! I will come once you come back"
