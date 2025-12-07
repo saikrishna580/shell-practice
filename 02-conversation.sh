76a4da4587964f18cf0c7d9456045c9dd5fa6bbe
saikowru <sai@devops.com>
2025-12-08 02:10:36 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+
+echo "trump:: Hey musk, How are you?"
+echo "musk":: Hello trump, I am fine. how are you doing"
+echo "trump":: I am fine too. whats's up?
+echo "musk" :: Nothing, just going to mars now, will you come"
+echo "trump" :: soory, you carry on! I will come once you come bacck"
