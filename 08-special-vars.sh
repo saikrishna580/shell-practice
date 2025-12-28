596117745059d964d55cf2f0b24ece0b44db76d2
saikowru <sai@devops.com>
2025-12-29 01:03:10 +0530

    special vars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -2,3 +2,10 @@
 
 echo "All variables paased to the script : $@"
 echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of the user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
