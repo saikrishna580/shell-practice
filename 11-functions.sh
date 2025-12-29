7cb3b9a80601611d1356bc8f221f18320c104d3c
saikowru <sai@devops.com>
2025-12-30 04:13:22 +0530

    special vars

---
+++ 11-functions.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit  #1 give other then 0 upto 127
+else
+    echo "You are running with root access"
+fi
+
+VALIDATE(){
+     if [ $1 -eq 0 ]
+    then
+        echo "Installing $2 is ... SUCCESS"
+    else
+        echo "Installing $2 is ... FAILURE"
+        exit 1
+    fi   
+
+}
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo "Mysql is not installed... going to install it"
+    dnf installed mysql
+    VALIDATE $? "mysql"
+else
+    echo "Mysql is already installed...Nothimg to do"
+ fi 
+
+ dnf list installed phython3
+if [ $? -ne 0 ]
+then 
+    echo "phython3 is not installed... going to install it"
+    dnf installed phython3
+    VALIDATE $? "python3"
+else
+    echo "phython3 is already installed...Nothimg to do"
+ fi
+
+ dnf list installed nginx
+if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed... going to install it"
+    dnf installed nginx
+    VALIDATE $? "ngnix"
+else
+    echo "nginx is already installed...Nothimg to do"
+ fi
